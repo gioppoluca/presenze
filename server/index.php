5e79b725c4b094e14c490a660d32ebe97b514b5a
Luca Gioppo <gioppoluca@libero.it>
2014-03-30 23:59:12 +0200

    initial commit

---
+++ server/index.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Step 1: Require the Slim Framework
+ *
+ * If you are not using Composer, you need to require the
+ * Slim Framework and register its PSR-0 autoloader.
+ *
+ * If you are using Composer, you can skip this step.
+ */
+require 'Slim/Slim.php';
+require 'RedBean/rb.phar';
+
+\Slim\Slim::registerAutoloader();
+
+// set up database connection
+R::setup('mysql:host=localhost;dbname=appdata','user','pass');
+R::freeze(true);
+
+/**
+ * Step 2: Instantiate a Slim application
+ *
+ * This example instantiates a Slim application using
+ * its default settings. However, you will usually configure
+ * your Slim application now by passing an associative array
+ * of setting names and values into the application constructor.
+ */
+$app = new \Slim\Slim();
+
+/**
+ * Step 3: Define the Slim application routes
+ *
+ * Here we define several Slim application routes that respond
+ * to appropriate HTTP request methods. In this example, the second
+ * argument for `Slim::get`, `Slim::post`, `Slim::put`, `Slim::patch`, and `Slim::delete`
+ * is an anonymous function.
+ */
+
+// GET route
+// route middleware for simple API authentication
+function authenticate(\Slim\Route $route) {
+    $app = \Slim\Slim::getInstance();
+    $uid = $app->getEncryptedCookie('uid');
+    $key = $app->getEncryptedCookie('key');
+    if (validateUserKey($uid, $key) === false) {
+      $app->halt(401);
+    }
+}
+
+function validateUserKey($uid, $key) {
+  // insert your (hopefully more complex) validation routine here
+  if ($uid == 'demo' && $key == 'demo') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// handle GET requests for /articles
+$app->get('/courses', 'authenticate', function () use ($app) {  
+  // query database for all articles
+  $articles = R::find('articles'); 
+  
+  // send response header for JSON content type
+  $app->response()->header('Content-Type', 'application/json');
+  
+  // return JSON-encoded response body with query results
+  echo json_encode(R::exportAll($articles));
+});
+
+// generates a temporary API key using cookies
+// call this first to gain access to protected API methods
+$app->get('/demo', function () use ($app) {    
+  try {
+    $app->setEncryptedCookie('uid', 'demo', '5 minutes');
+    $app->setEncryptedCookie('key', 'demo', '5 minutes');
+  } catch (Exception $e) {
+    $app->response()->status(400);
+    $app->response()->header('X-Status-Reason', $e->getMessage());
+  }
+});
+
+/**
+ * Step 4: Run the Slim application
+ *
+ * This method should be called last. This executes the Slim application
+ * and returns the HTTP response to the HTTP client.
+ */
+$app->run();
